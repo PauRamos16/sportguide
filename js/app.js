a4361e1d4dd1ae2f9c2abd1bfa46952cfead5063
maramos <maramos@dipta.cat>
2024-05-23 10:48:23 +0200

    sportguide proposta novetat i espai

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-w3.includeHTML();
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  /* test console log */
+  console.log("Hello PAU16!");
+
+  /* includes html static html */
+  w3.includeHTML();
+
+  /* slider or slideshow */
+  var slider = document.querySelectorAll('.slider__image');
+  if (slider.length > 0) {
+    w3.slideshow(".slider__image", 4000);
+  }
+
+  /* mapes */
+  var mapa = document.querySelectorAll('.map__wrap');
+  if (mapa.length > 0) {
+    var map = L.map('map').setView([41.1438632,1.1106476], 13);
+    L.tileLayer('https://geoserveis.icgc.cat/servei/catalunya/contextmaps/wmts/contextmaps-mapa-estandard/MON3857NW/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; Catalunya: Institut Cartogràfic i Geològic de Catalunya <a href="https://openicgc.github.io/">IGCC</a>'
+    }).addTo(map);
+  }
+  
+  
+});
+
